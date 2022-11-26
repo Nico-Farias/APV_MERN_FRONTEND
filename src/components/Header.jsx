7a6e06d8917c0f8931f792ec9590ab5ba39fb3e0
Nico-Farias <nicoscs12@gmail.com>
2022-11-25 21:05:55 -0300

    actualizar nombre

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
 
         <header className='py-10 bg-indigo-600'>
             <div className='container mx-auto flex justify-between items-center flex-col lg:flex-row'>
-                <h1 className='font-bold text-2xl text-center text-indigo-200'>Administrador de paciente de {''} <span className='text-white font-black'>Veterinaria</span></h1>
+                <h1 className='font-bold text-2xl text-center text-indigo-200'>Administrador de Paciente de {''} <span className='text-white font-black'>Veterinaria</span></h1>
 
                 <nav className='flex gap-4 flex-col items-center lg:flex-row mt-5 lg:mt-0'>
                     <Link to={"/admin"} className='text-white text-xl uppercase font-bold'>Pacientes</Link>
